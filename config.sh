36a7e9e59774f87f2714539d59c02b1b94d7fecb
shengsli <sheng.s.li@outlook.com>
2019-03-25 18:50:02 +0000

    update

--- config.sh
+++ config.sh
@@ -3,7 +3,8 @@ git config --global user.email sheng.s.li@outlook.com
 git config --global credential.helper store
 sudo bash -c "echo 100 > /sys/devices/platform/i8042/serio1/serio2/sensitivity"
 # sudo pacman -S emacs vim aspell aspell-en feh texlive-core biber texlive-latexextra firefox evince texlive-pictures texlive-bibtexextra
-cp ~/dev/doti3/.i3/config ~/.i3/config
+mkdir -p ~/.config/i3/
+cp ~/dev/doti3/.i3/config ~/.config/i3/config
 cp ~/dev/doti3/.i3status.conf ~/.i3status.conf
 cp ~/dev/doti3/.Xresources ~/
 xrdb ~/.Xresources

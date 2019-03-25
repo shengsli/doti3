899a9b7ed8ebdfe451f00ef02ce638e3cbf8a79a
shengsli <sheng.s.li@outlook.com>
2019-03-25 18:12:46 +0000

    update

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@ git config --global user.name shengsli
 git config --global user.email sheng.s.li@outlook.com
 git config --global credential.helper store
 sudo bash -c "echo 100 > /sys/devices/platform/i8042/serio1/serio2/sensitivity"
-sudo pacman -S emacs vim aspell aspell-en feh texlive-core biber texlive-latexextra firefox evince texlive-pictures texlive-bibtexextra
+# sudo pacman -S emacs vim aspell aspell-en feh texlive-core biber texlive-latexextra firefox evince texlive-pictures texlive-bibtexextra
 cp .i3/config ~/.i3/
 cp .i3status.conf ~/.i3status.conf
 cp .Xresources ~/
@@ -12,4 +12,5 @@ export EDITOR=/usr/bin/emacs
 sudo mkdir -p /usr/share/backgrounds/archlinux/
 sudo cp archlinux-simplyblack.png /usr/share/backgrounds/archlinux/
 feh --bg-scale /usr/share/backgrounds/archlinux/archlinux-simplyblack.png 
-sudo pacman -R palemoon-bin
+sudo cp ~/dev/doti3/etc/lightdm/slick-greeter.conf /etc/lightdm/
+# sudo pacman -R palemoon-bin

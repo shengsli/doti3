46f3be8a3c7bc3fef499cefd00894cf864a6ba31
shengsli <sheng.s.li@outlook.com>
2019-03-25 00:13:09 +0000

    update

--- config.sh
+++ config.sh
@@ -1,3 +1,11 @@
+sudo pacman -S emacs vim aspell aspell-en feh texlive-core biber texlive-latexextra firefox
+cp .i3/config ~/.i3/
+cp .i3status.conf ~/.i3status.conf
+cp .Xresources ~/
+xrdb ~/.Xresources
+cp .config/mimeapps.list ~/.config/mimeapps.list
 export EDITOR=/usr/bin/emacs
+sudo mkdir -p /usr/share/backgrounds/archlinux/
+sudo cp archlinux-simplyblack.png /usr/share/backgrounds/archlinux/
 feh --bg-scale /usr/share/backgrounds/archlinux/archlinux-simplyblack.png 
-cp ./.config/mimeapps.list ~/.config/mimeapps.list
+sudo pacman -R palemoon-bin
